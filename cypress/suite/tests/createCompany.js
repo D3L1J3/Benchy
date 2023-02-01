50efc96597d857dafc1d09e1702f0bc2c56eef88
D3L1J3 <37899526+D3L1J3@users.noreply.github.com>
2023-02-01 09:30:13 +0100

    changed xpath selectors

--- cypress/suite/tests/createCompany.js
+++ cypress/suite/tests/createCompany.js
@@ -5,18 +5,19 @@
 describe("Test company creation", () => {
     it("Should be able to submit a successful submission creating a company form", () => {
         // Returns a random integer from 1 to 999999:
-        const ranInt   =   Math.floor(Math.random() * 1000000) + 100000
+        const ranInt   =   Math.floor(Math.random() * 1000000) + 100000;
         
-        const orgNum    =   ranInt
-        const fullName  =   "compName"
-        const email     =   ranInt + "@gmail.com"
-        const pwd       =   "compPwd"
-        const address  =   "compAddress"
-        const city      =   "compCity"
-        const zip       =   "compZip"
-        const country   =   "compCountry"
+        const orgNum    =   ranInt;
+        const fullName  =   "compName";
+        const email     =   ranInt + "@gmail.com";
+        const pwd       =   "compPwd";
+        const address  =   "compAddress";
+        const city      =   "compCity";
+        const zip       =   "compZip";
+        const country   =   "compCountry";
+        const companyDesc = "Lorem ipsum dolor sit amet, consectetur adipiscing magna aliqua";
 
-        cy.visit('https://benchy.se/auth/welcome')
+        cy.visit('https://benchy.se/auth/welcome');
         cy.xpath("//img[@alt='Company']").click();
         cy.contains('Create account').click();
         cy.get('#company_name').type(ranInt);
@@ -35,6 +36,18 @@ describe("Test company creation", () => {
         cy.xpath("//button[contains(text(), 'Create account')]").click();
         cy.url().should('include', '/company/setup-profile');
         cy.contains('Set up company profile');
+
+        // // Adds a profile picture and verify that it is uploaded
+        cy.xpath("//label[@role='button']").first().selectFile('cypress/fixtures/pictures/profilePic.png');
+        cy.request('*.png')
+            .its('status')
+            .should('equal', 200);
+        cy.xpath("//img[@alt='Profile picture']").should('be.visible');
+
+        // Adds company description and clicks the "Complete your profile" button
+        cy.get('#company_description').type(companyDesc);   
+        cy.xpath("//button[contains(text(), 'Complete your profile')]").click();
+        cy.url().should('include', '/company/home');
     });
 })
 
--- cypress/suite/tests/loginCompany.js
+++ cypress/suite/tests/loginCompany.js
@@ -4,20 +4,23 @@
 
 describe("Test company login", () => {
     it("Should be able to login to a company account", () => {
-        cy.visit('https://benchy.se/auth/welcome')
-        cy.xpath("//a[@href='/auth/login']/button[@type='button']").click();
+        cy.visit('https://benchy.se/auth/welcome');
+        cy.xpath("//button[contains(text(), 'Log in')]").click();
         cy.url().should('include', '/auth/login');
         cy.get('#email').type('140838@gmail.com');
         cy.get('#password').type('compPwd');
         cy.xpath("//button[contains(text(), 'Sign in')]").click();
-        cy.url().should('include', '/company/setup-profile');
-        
-        // Find consultants
+
+        // Verify url and profile image
+        cy.url().should('include', '/company/home'); 
+        cy.xpath("//img[@alt='logo']").should('be.visible');
+
+        // Find consultants and verifies that match exists
         cy.contains('Find Consultants').click();
         cy.url().should('include', '/company/consultant-listings');
         cy.xpath("//input[@id='tags-outlined']").first().click();
         cy.contains('JavaScript (Programming Language)').click();
-        cy.xpath("//button[@type='submit']").click();
+        cy.xpath("//button[contains(text(), 'Search')]").click();
         cy.contains(/Here are \d+ consultants/);
         cy.xpath("//img[@alt='Applicant']").should('be.visible');
     });
